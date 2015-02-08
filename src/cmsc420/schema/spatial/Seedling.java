f46f7638ea358e2940040ffb05cee08ee812a76e
Andrew Liu <liu.andrew.x@gmail.com>
2015-02-08 11:59:18 -0500

    noted why I added seedlings

--- src/cmsc420/schema/spatial/Seedling.java
+++ src/cmsc420/schema/spatial/Seedling.java
@@ -1,5 +1,13 @@
 package cmsc420.schema.spatial;
 
+/**
+ * @author Andrew Liu
+ * 
+ *         The decision to make seedlings to get instances of trees was based
+ *         off the fact that I needed an instance of the tree before I had the
+ *         necessary data (width and height) to support the tree.
+ *
+ */
 public interface Seedling {
 
 	public SpatialStructure generate(float width, float height);
