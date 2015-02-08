1c1fb60fc7dfc81407e7a03f291690f9a63856a4
Andrew Liu <liu.andrew.x@gmail.com>
2015-02-07 23:46:46 -0500

    implemented city coordinate comparator

--- src/cmsc420/schema/CityCoordinateComparator.java
+++ src/cmsc420/schema/CityCoordinateComparator.java
@@ -7,7 +7,19 @@ public class CityCoordinateComparator implements Comparator<City> {
 	@Override
 	public int compare(City o1, City o2) {
 		// TODO Auto-generated method stub
-		return 0;
+		if (o1.x == o2.x) {
+			if (o1.y == o2.y) {
+				return 0;
+			} else if (o1.x < o2.x) {
+				return -1;
+			} else { // o1.y > o2.y
+				return 1;
+			}
+		} else if (o1.x < o2.x) {
+			return -1;
+		} else { // o1.x > o2.x
+			return 1;
+		}
 	}
 
 }
