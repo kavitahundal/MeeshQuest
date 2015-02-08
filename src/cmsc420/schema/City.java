5969183948e2a6720f108337eb99d0ba5d0c356c
Andrew Liu <liu.andrew.x@gmail.com>
2015-02-08 00:03:41 -0500

    implemented dictionary and city

--- src/cmsc420/schema/City.java
+++ src/cmsc420/schema/City.java
@@ -8,5 +8,20 @@ public class City extends Float {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private String name;
+	private CityColor color;
+	
+	public City(String name, float x, float y, CityColor color) {
+		super(x, y);
+		this.name = name;
+		this.color = color;
+	}
 
+	public String getName() {
+		return this.name;
+	}
+	
+	public CityColor getColor() {
+		return this.color;
+	}
 }
--- src/cmsc420/schema/dictionary/TreeMapDictionary.java
+++ src/cmsc420/schema/dictionary/TreeMapDictionary.java
@@ -1,5 +1,39 @@
 package cmsc420.schema.dictionary;
 
+import java.util.TreeMap;
+
+import cmsc420.schema.City;
+import cmsc420.schema.CityCoordinateComparator;
+import cmsc420.schema.CityNameComparator;
+
 public class TreeMapDictionary {
 
+	private TreeMap<String, City> nameToCity;
+	private TreeMap<City, String> cityToName;
+	
+	public TreeMapDictionary() {
+		this.nameToCity = new TreeMap<>(new CityNameComparator());
+		this.cityToName = new TreeMap<>(new CityCoordinateComparator());
+	}
+	
+	public void insert(City city) {
+		// TODO what if coordinates are the same?
+		this.nameToCity.put(city.getName(), city);
+		this.cityToName.put(city, city.getName());
+	}
+	
+	public boolean containsName(String name) {
+		return this.nameToCity.containsKey(name);
+	}
+	
+	public boolean containsCoordinates(City city) {
+		return this.cityToName.containsKey(city); // equals for city must only check coords!!!
+		// in other words, don't override equals :)
+	}
+	
+	public void delete(City city) {
+		// precondition: contains returns true
+		this.nameToCity.remove(city.getName());
+		this.cityToName.remove(city);
+	}
 }
