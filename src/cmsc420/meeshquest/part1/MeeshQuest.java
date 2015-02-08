c878de5e4036da8bb523b1e3f200a3938683e1dc
Andrew Liu <liu.andrew.x@gmail.com>
2015-02-08 01:11:57 -0500

    design shift from driver to mediator

--- src/cmsc420/meeshquest/part1/MeeshQuest.java
+++ src/cmsc420/meeshquest/part1/MeeshQuest.java
@@ -1,97 +1,14 @@
 package cmsc420.meeshquest.part1;
 
-import java.io.IOException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import cmsc420.schema.CityColor;
-import cmsc420.schema.SortType;
-import cmsc420.xml.XmlUtility;
+import cmsc420.schema.dictionary.TreeMapDictionary;
+import cmsc420.schema.mediator.CommandParser;
+import cmsc420.schema.spatial.PRQuadTree;
 
 public class MeeshQuest {
 
 	public static void main(String[] args) {
-
-		/* XML documents for input and output */
-		Document doc = null;
-		Document results = null;
-
-		/* initialize output XML document */
-		try {
-			results = XmlUtility.getDocumentBuilder().newDocument();
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace(); // should never occur
-		}
-		/* validate input XML */
-		try {
-			doc = XmlUtility.validateNoNamespace(System.in);
-			Node root = doc.getFirstChild(); // root tag from XML document
-			NodeList commands = root.getChildNodes(); // command tags
-			Element resultsRoot = results.createElement("results");
-			results.appendChild(resultsRoot); // root tag of XML output
-
-			/* retrieve spatial attributes */
-			NamedNodeMap attrs = root.getAttributes();
-			int spatialWidth = Integer.parseInt(attrs.getNamedItem("spatialWidth").getNodeValue());
-			int spatialHeight = Integer.parseInt(attrs.getNamedItem("spatialHeight").getNodeValue());
-
-			/* process each command */
-			for (int i = 0; i < commands.getLength(); i++) {
-				Node commandNode = commands.item(i);
-				if (commandNode instanceof Element) {
-					String command = commandNode.getNodeName();
-					NamedNodeMap params = commandNode.getAttributes();
-					if (command.equals("createCity")) {
-						String name = params.getNamedItem("name").getNodeValue();
-						int x = Integer.parseInt(params.getNamedItem("x").getNodeValue());
-						int y = Integer.parseInt(params.getNamedItem("y").getNodeValue());
-						int radius = Integer.parseInt(params.getNamedItem("radius").getNodeValue());
-						CityColor color = CityColor.getCityColor(params.getNamedItem("color").getNodeValue());
-					} else if (command.equals("deleteCity")) {
-						String name = params.getNamedItem("name").getNodeValue();
-					} else if (command.equals("clearAll")) {
-						// no params
-					} else if (command.equals("listCities")) {
-						SortType sortBy = SortType.getSortType(params.getNamedItem("sortBy").getNodeValue());
-					} else if (command.equals("mapCity")) {
-						String name = params.getNamedItem("name").getNodeValue();
-					} else if (command.equals("unmapCity")) {
-						String name = params.getNamedItem("name").getNodeValue();
-					} else if (command.equals("printPRQuadtree")) {
-						// no params
-					} else if (command.equals("saveMap")) {
-						String name = params.getNamedItem("name").getNodeValue();
-					} else if (command.equals("rangeCities")) {
-						int x = Integer.parseInt(params.getNamedItem("x").getNodeValue());
-						int y = Integer.parseInt(params.getNamedItem("y").getNodeValue());
-						int radius = Integer.parseInt(params.getNamedItem("radius").getNodeValue());
-						String name = params.getNamedItem("name").getNodeValue();
-					} else if (command.equals("nearestCity")) {
-						int x = Integer.parseInt(params.getNamedItem("x").getNodeValue());
-						int y = Integer.parseInt(params.getNamedItem("y").getNodeValue());
-					}
-				}
-			}
-		} catch (SAXException | IOException | ParserConfigurationException e) {
-			/* create fatal error tag */
-			Element elt = results.createElement("fatalError");
-			results.appendChild(elt);
-		}
-
-		/* print output XML document */
-		try {
-			XmlUtility.print(results);
-		} catch (TransformerException e) {
-			e.printStackTrace(); // should never occur
-		}
-
+		CommandParser mediator = new CommandParser(new TreeMapDictionary(), new PRQuadTree(), null);
+		mediator.process();
+		mediator.print();
 	}
 }
---
+++ src/cmsc420/schema/mediator/CommandParser.java
@@ -0,0 +1,123 @@
+package cmsc420.schema.mediator;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import cmsc420.schema.CityColor;
+import cmsc420.schema.SortType;
+import cmsc420.schema.adjacencylist.AdjacencyListStructure;
+import cmsc420.schema.dictionary.DictionaryStructure;
+import cmsc420.schema.spatial.SpatialStructure;
+import cmsc420.xml.XmlUtility;
+
+public class CommandParser {
+
+	private Document input;
+	private Document output;
+	private float spatialWidth;
+	private float spatialHeight;
+	private DictionaryStructure dictionary;
+	private SpatialStructure spatial;
+	private AdjacencyListStructure adjacencyList;
+	private boolean processed;
+	private Element results;
+	
+	// TODO documentation, print success/error, classes for each command???
+
+	public CommandParser(DictionaryStructure dictionary, SpatialStructure spatial, AdjacencyListStructure adjacencyList) {
+		this.processed = false;
+		
+		/* initialize data structures */
+		this.dictionary = dictionary;
+		this.spatial = spatial;
+		this.adjacencyList = adjacencyList;
+		
+		/* initialize output XML document */
+		try {
+			this.output = XmlUtility.getDocumentBuilder().newDocument();
+		} catch (ParserConfigurationException e) {
+		}
+	}
+	
+	public void process() {
+		if (!this.processed) {
+			this.processed = true;
+			try {
+				input = XmlUtility.validateNoNamespace(System.in);
+				results = output.createElement("results");
+				output.appendChild(results); // root tag of XML output
+				this.parse();
+			} catch (SAXException | IOException | ParserConfigurationException e) {
+				output.appendChild(output.createElement("fatalError"));
+			}
+		}
+	}
+	
+	private void parse() {
+		//
+		Node root = input.getFirstChild(); // root tag from XML document
+		NodeList commands = root.getChildNodes(); // command tags
+		
+
+		/* retrieve spatial attributes */
+		NamedNodeMap attrs = root.getAttributes();
+		spatialWidth = Integer.parseInt(attrs.getNamedItem("spatialWidth").getNodeValue());
+		spatialHeight = Integer.parseInt(attrs.getNamedItem("spatialHeight").getNodeValue());
+
+		/* process each command */
+		for (int i = 0; i < commands.getLength(); i++) {
+			Node commandNode = commands.item(i);
+			if (commandNode instanceof Element) {
+				String command = commandNode.getNodeName();
+				NamedNodeMap params = commandNode.getAttributes();
+				if (command.equals("createCity")) {
+					String name = params.getNamedItem("name").getNodeValue();
+					int x = Integer.parseInt(params.getNamedItem("x").getNodeValue());
+					int y = Integer.parseInt(params.getNamedItem("y").getNodeValue());
+					int radius = Integer.parseInt(params.getNamedItem("radius").getNodeValue());
+					CityColor color = CityColor.getCityColor(params.getNamedItem("color").getNodeValue());
+				} else if (command.equals("deleteCity")) {
+					String name = params.getNamedItem("name").getNodeValue();
+				} else if (command.equals("clearAll")) {
+					// no params
+				} else if (command.equals("listCities")) {
+					SortType sortBy = SortType.getSortType(params.getNamedItem("sortBy").getNodeValue());
+				} else if (command.equals("mapCity")) {
+					String name = params.getNamedItem("name").getNodeValue();
+				} else if (command.equals("unmapCity")) {
+					String name = params.getNamedItem("name").getNodeValue();
+				} else if (command.equals("printPRQuadtree")) {
+					// no params
+				} else if (command.equals("saveMap")) {
+					String name = params.getNamedItem("name").getNodeValue();
+				} else if (command.equals("rangeCities")) {
+					int x = Integer.parseInt(params.getNamedItem("x").getNodeValue());
+					int y = Integer.parseInt(params.getNamedItem("y").getNodeValue());
+					int radius = Integer.parseInt(params.getNamedItem("radius").getNodeValue());
+					String name = params.getNamedItem("name").getNodeValue();
+				} else if (command.equals("nearestCity")) {
+					int x = Integer.parseInt(params.getNamedItem("x").getNodeValue());
+					int y = Integer.parseInt(params.getNamedItem("y").getNodeValue());
+				}
+			}
+		}
+	}
+	
+	public void print() {
+		if (this.processed) {
+			try {
+				XmlUtility.print(this.output);
+			} catch (TransformerException e) {
+			}	
+		}
+	}
+}
