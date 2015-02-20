29f1044bd810340259deb91f97834b60a1c20d0e
Andrew Liu <liu.andrew.x@gmail.com>
2015-02-20 13:55:06 -0500

    some implementation of submap

--- src/cmsc420/sortedmap/AvlGTree.java
+++ src/cmsc420/sortedmap/AvlGTree.java
@@ -127,7 +127,7 @@ public class AvlGTree<K, V> implements SortedMap<K, V> {
 		}
 		this.balanceCheck(entry);
 	}
-	
+
 	private void balanceCheck(Entry<K, V> entry) {
 		int balanceFactor = this.balanceFactor(entry);
 		if (Math.abs(balanceFactor) > this.g) {
@@ -176,7 +176,7 @@ public class AvlGTree<K, V> implements SortedMap<K, V> {
 			}
 		}
 	}
-	
+
 	private void swapData(Entry<K, V> first, Entry<K, V> second) {
 		K tempKey = first.key;
 		V tempVal = first.value;
@@ -185,15 +185,15 @@ public class AvlGTree<K, V> implements SortedMap<K, V> {
 		second.key = tempKey;
 		second.value = tempVal;
 	}
-	
+
 	private int height(Entry<K, V> entry) {
 		return entry == null ? 0 : entry.height;
 	}
-	
+
 	private void updateHeight(Entry<K, V> entry) {
 		entry.height = Math.max(height(entry.left), height(entry.right));
 	}
-	
+
 	private int balanceFactor(Entry<K, V> entry) {
 		return height(entry.left) - height(entry.right);
 	}
@@ -271,8 +271,7 @@ public class AvlGTree<K, V> implements SortedMap<K, V> {
 		if (comp.compare(arg0, arg1) > 0) {
 			throw new IllegalArgumentException();
 		}
-		// TODO Auto-generated method stub
-		return null;
+		return new AvlSubMap(this, arg0, arg1);
 	}
 
 	@Override
@@ -305,7 +304,6 @@ public class AvlGTree<K, V> implements SortedMap<K, V> {
 		}
 
 		@Override
-		
 		public V getValue() {
 			return this.value;
 		}
@@ -315,6 +313,160 @@ public class AvlGTree<K, V> implements SortedMap<K, V> {
 			V oldValue = this.value;
 			this.value = value;
 			return oldValue;
+		}
+		
+		public int yes() {
+			return 45;
+		}
+
+	}
+
+	class AvlSubMap implements SortedMap<K, V> {
+
+		private AvlGTree<K, V> wrapper;
+		private K lowerBound;
+		private K upperBound;
+		private int size;
+
+		AvlSubMap(AvlGTree<K, V> wrapper, K lowerBound, K upperBound) {
+			this.wrapper = wrapper;
+			this.lowerBound = lowerBound;
+			this.upperBound = upperBound;
+			this.size = this.calculateSize(this.wrapper.root);
+		}
+		
+		private int calculateSize(AvlGTree.Entry<K, V> entry) {
+			if (entry == null) {
+				return 0;
+			}
+			return this.calculateSize(entry.left) + this.calculateSize(entry.right) + 1;
+		}
+
+		private boolean inBounds(Object arg0) {
+			@SuppressWarnings("unchecked")
+			K key = (K) arg0;
+			return this.wrapper.comparator().compare(this.lowerBound, key) <= 0
+					&& this.wrapper.comparator().compare(key, this.upperBound) < 0;
+		}
+
+		@Override
+		public void clear() {
+			this.wrapper.clear();
+		}
+
+		@Override
+		public boolean containsKey(Object arg0) {
+			return this.inBounds(arg0) ? this.wrapper.containsKey(arg0) : false;
+		}
+
+		@Override
+		public boolean containsValue(Object arg0) {
+			return this.inBounds(arg0) ? this.wrapper.containsValue(arg0) : false;
+		}
+
+		@Override
+		public V get(Object arg0) {
+			return this.inBounds(arg0) ? this.wrapper.get(arg0) : null;
+		}
+
+		@Override
+		public boolean isEmpty() {
+			return this.size == 0;
+		}
+
+		@Override
+		public V put(K arg0, V arg1) {
+//			if (arg0 == null || arg1 == null) {
+//				throw new NullPointerException();
+//			}
+//			AvlGTree.Entry<K, V> entry = this.wrapper.findEntry(arg0);
+//			if (entry != null) {
+//				V ret = entry.value;
+//				entry.value = arg1;
+//				return ret;
+//			} else {
+//				this.size++;
+//				this.add(new AvlGTree.Entry<K, V>(arg0, arg1));
+//				return null;
+//			}
+			// TODO
+			return null;
+		}
+
+		@Override
+		public void putAll(Map<? extends K, ? extends V> arg0) {
+			if (arg0 == null) {
+				throw new NullPointerException();
+			}
+			for (K key : arg0.keySet()) {
+				this.put(key, arg0.get(key));
+			}
+		}
+
+		@Override
+		public V remove(Object arg0) {
+			throw new UnsupportedOperationException(); // implement in part 3
+		}
+
+		@Override
+		public int size() {
+			return this.size;
+		}
+
+		@Override
+		public Comparator<? super K> comparator() {
+			return this.wrapper.comparator();
+		}
+
+		@Override
+		public Set<java.util.Map.Entry<K, V>> entrySet() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public K firstKey() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public SortedMap<K, V> headMap(K arg0) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public Set<K> keySet() {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public K lastKey() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public SortedMap<K, V> subMap(K arg0, K arg1) {
+			if (arg0 == null || arg1 == null) {
+				throw new NullPointerException();
+			}
+			if (comp.compare(arg0, arg1) > 0) {
+				throw new IllegalArgumentException();
+			}
+			K lower = this.wrapper.comparator().compare(this.lowerBound, arg0) > 0 ? this.lowerBound : arg0;
+			K upper = this.wrapper.comparator().compare(this.upperBound, arg1) < 0 ? this.upperBound : arg1;
+			return new AvlSubMap(this.wrapper, lower, upper);
+		}
+
+		@Override
+		public SortedMap<K, V> tailMap(K arg0) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public Collection<V> values() {
+			throw new UnsupportedOperationException();
 		}
 
 	}
