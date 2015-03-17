bfe8efe8695018f80524748a20b530810725ce82
Andrew Liu <liu.andrew.x@gmail.com>
2015-03-17 01:51:54 -0400

    added exception classes

---
+++ src/cmsc420/exceptions/NonExistentStartException.java
@@ -0,0 +1,13 @@
+package cmsc420.exceptions;
+
+public class NonExistentStartException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NonExistentStartException() {
+		super("nonExistentStart");
+	}
+}
---
+++ src/cmsc420/exceptions/RoadNotFoundException.java
@@ -0,0 +1,13 @@
+package cmsc420.exceptions;
+
+public class RoadNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public RoadNotFoundException() {
+		super("roadNotFound");
+	}
+}
